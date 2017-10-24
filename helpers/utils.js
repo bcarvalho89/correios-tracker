64841f910cbaaa07ea1e76af60e0b9064ba72657
Bruno Carvalho <bruno@brunocarvalho.me>
2017-10-24 09:29:03 -0200

    Add object validator and better error handling

--- helpers/utils.js
+++ helpers/utils.js
@@ -11,3 +11,21 @@ export function normalizePort(val) {
 
   return false;
 }
+
+export function validateSRO(object){
+  let objectRegex = /^((BJ|NN|CF|CM|CX|ML|PL|PW|SB|TT|PE|MR|UA)[0-9]{9}|[A-Za-z]{2}[0-9]{9}[A-Za-z]{2}|[N]{2}[0-9]{11})$/;
+  object = cleanString(object.toUpperCase());
+
+  if (object.length === 0) {
+    //console.log(`${object} - Object empty`);
+    return false;
+  } else {
+    if (objectRegex.test(object)) {
+      //console.log(`${object} - Object valid`);
+      return true;
+    } else {
+      //console.log(`${object} - Object invalid`);
+      return false;
+    }
+  }
+}

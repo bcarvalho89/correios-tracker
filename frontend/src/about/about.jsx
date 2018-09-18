10e4f1d2656bb693eb594c949bd3165a41051772
Bruno Carvalho <bruno.gomes@matera.com>
2018-09-18 13:46:34 -0300

    Add text in about page

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <h1>Jaiminho</h1>
+      <div>
+        <h2>Jaiminho é um projeto desenvolvido para rastrear encomendas através dos correios de uma forma rápida e fácil.</h2><br/>
+        <p>Utilizamos o webservice dos correios para obter as informações e as mesmas não são armazenadas.</p><br/>
+        <p><strong>Dica: Você pode adicionar a url gerada na consulta em seus favoritos ;)</strong></p>
+      </div>
     )
   }
 }
